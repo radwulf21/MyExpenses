46b3a23a14c5c657c38c5dcae530b58d1303daf5
Raul Andrade <raul.andrade21@gmail.com>
2021-11-20 19:05:15 -0300

    Adjusting view binding

--- app/src/main/java/com/example/myexpenses/view/MainActivity.kt
+++ app/src/main/java/com/example/myexpenses/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.myexpenses.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.myexpenses.databinding.ActivityMainBinding
@@ -10,7 +9,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private val months = listOf("A", "B", "C", "D", "E", "F", "G")
+    private val months = listOf("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
 
     private lateinit var binding: ActivityMainBinding
 
@@ -20,14 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.vpExpenses.adapter = FragmentExpensesAdapter(this)
+        binding.vpExpenses.isUserInputEnabled = false
 
         TabLayoutMediator(binding.tbMonths, binding.vpExpenses) { tab, position ->
             tab.text = months[position]
         }.attach()
     }
-}
 
-class FragmentExpensesAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-    override fun createFragment(position: Int) = ExpensesFragment()
-    override fun getItemCount() = 7
+    inner class FragmentExpensesAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun createFragment(position: Int) = ExpensesFragment()
+        override fun getItemCount() = 12
+    }
 }
---
+++ app/src/main/java/com/example/myexpenses/viewmodel/ExpensesViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.myexpenses.viewmodel
+
+class ExpensesViewModel {
+
+}
